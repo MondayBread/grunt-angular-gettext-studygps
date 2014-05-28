d53de4323c181855e527fe879a9c75197faddb0a
Yun YoungSik <ysyun@yuwin.co.kr>
2014-05-28 23:11:38 +0900

    add message task

--- tasks/compile.js
+++ tasks/compile.js
@@ -18,4 +18,22 @@ module.exports = function (grunt) {
             grunt.file.write(file.dest, compiler.convertPo(inputs));
         });
     });
+
+    grunt.registerMultiTask('nggettext_msg_compile', 'Compile strings from .po files', function () {
+        var options = this.options();
+
+        if (options.format && !Compiler.hasFormat(options.format)) {
+            throw new Error('There is no "' + options.format + '" output format.');
+        }
+
+        this.files.forEach(function (file) {
+            var inputs = file.src.map(function (input) {
+                return grunt.file.read(input);
+            });
+
+            var compiler = new Compiler(options);
+
+            grunt.file.write(file.dest, compiler.convertPo(inputs));
+        });
+    });
 };
